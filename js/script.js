e37e4058ab1a935d1377df2901211259c83f6b95
Rendi <rendyoktavian08@gmail.com>
2024-02-22 15:01:00 +0700

    Undangan Aqiqah

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@
 
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
-    // console.log('Shaadi me zaroor aana');
 });
 
 // Set the date we're counting down to
@@ -21,13 +20,13 @@ var x = setInterval(function() {
     var distance = countDownDate - now;
     
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var hari = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var jam = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var menit = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var detik = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+    document.getElementById("time").innerHTML = "<div class='container'><div class='hari block'>"+ hari + "<br>Hari</div>" + "<div class='jam block'>" + jam + "<br>Jam</div>" + "<div class='menit block'>" + menit + "<br>Menit</div>" + "<div class='detik block'>" + detik + "<br>Detik</div></div>";
     
     // If the count down is over, write some text 
     if (distance < 0) {
